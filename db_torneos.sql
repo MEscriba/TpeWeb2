1006e8067ab72947ae1b7929a566cfd8a6d85a4d
Martin Escriba Bab <martin_escriba@hotmail.com>
2022-11-12 10:43:42 -0300

    se reemplazo la base de datos

--- db_torneos.sql
+++ db_torneos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2022 a las 02:21:06
+-- Tiempo de generación: 12-11-2022 a las 14:41:51
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -31,68 +31,51 @@ CREATE TABLE `arbitros` (
   `id` int(11) NOT NULL,
   `foto` longblob NOT NULL,
   `arbitro` varchar(100) NOT NULL,
-  `asociacion` varchar(80) NOT NULL,
-  `region` varchar(80) NOT NULL
+  `residencia` varchar(80) NOT NULL,
+  `id_asociacion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `arbitros`
 --
 
-INSERT INTO `arbitros` (`id`, `foto`, `arbitro`, `asociacion`, `region`) VALUES
-(23, '', 'caetano', 'nuevo', 'tandil'),
-(24, '', 'Antonela Morales', 'Asociacion Marplatense', 'Bonaerense'),
-(25, '', 'Catalina Zaninovich', 'Asociacion Bahiense', 'Bonaerense'),
-(26, '', 'Moira Cuenca', 'Asociacion Pampeana', 'Pampeana'),
-(27, '', 'Mariana Soto', 'Rio Negro', 'Patagonica'),
-(28, '', 'Estefania Gonzalez Mons', 'Austral', 'Patagonica'),
-(29, '', 'Dolores Sanchez', 'Asociacion Hockey de Buenos Aires', 'Metropolitana'),
-(30, '', 'Florencia Gomez', 'Asociacion Hockey de Buenos Aires', 'Metropolitana'),
-(33, '', 'Claudio Alcaza', 'Federacion Tandilense', 'Bonaerense'),
-(34, '', 'Juan Pablo Dabrowski', 'Neuquen', 'Patagonica'),
-(35, '', 'Cristian Baigorria', 'Asociacion Marplatense', 'Bonaerense'),
-(36, '', 'Ezequiel Valusek', 'Asociacion del Centro', 'Bonaerense'),
-(37, '', 'Cecilia Gomez', 'Federacion de Santiago del Estero', 'NOA'),
-(38, '', 'Gisele Gonzalez', 'Asociacion de Litoral', 'NEA');
+INSERT INTO `arbitros` (`id`, `foto`, `arbitro`, `residencia`, `id_asociacion`) VALUES
+(59, '', 'prueba', 'Tandil', 48),
+(60, '', 'Nicolas Da Silva', 'Mar del Plata', 44),
+(61, '', 'Diego Barbas', 'Tandil', 43),
+(62, '', 'Fernando Grill', 'Mar del Plata', 44),
+(63, '', 'Cecilia Gomez', 'Tandil', 54),
+(64, '', 'Ezequiel Valusek', 'Henderson', 54),
+(65, '', 'Hernan Del ruiz', 'Bahia Blanca', 48),
+(66, '', 'Basilio Canale', 'Bahia Blanca', 48);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `equipos`
+-- Estructura de tabla para la tabla `asociaciones`
 --
 
-CREATE TABLE `equipos` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `asociaciones` (
+  `id_asociacion` int(11) NOT NULL,
   `logo` tinyblob NOT NULL,
-  `equipo` varchar(100) NOT NULL,
   `asociacion` varchar(80) NOT NULL,
   `region` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `equipos`
+-- Volcado de datos para la tabla `asociaciones`
 --
 
-INSERT INTO `equipos` (`id`, `logo`, `equipo`, `asociacion`, `region`) VALUES
-(8, '', 'Bahia Blanca', 'Asociacion Bahiense', 'Bonaerense'),
-(9, '', 'Tandil', 'Federacion Tandilense', 'Bonaerense'),
-(10, '', 'Mar del Plata', 'Asociacion Marplatense', 'Bonaerense'),
-(11, '', 'Austral', 'Austral', 'Patagonica'),
-(16, '', 'Uruguay', 'Federacion Uruguaya de Hockey', 'Otra'),
-(17, '', 'Sudoeste', 'Asociacion Sudoeste', 'Bonaerense'),
-(19, '', 'AHO', 'Asociación del Oeste de Bs.As.', 'Bonaerense'),
-(20, '', 'Noroeste', 'Asociación Noroeste', 'Pampeana'),
-(24, '', 'Catamarca', 'Federacion Catamarqueña de Hockey', 'Otra'),
-(25, '', 'Tres Arroyos', 'Asociación Regional Tresarroyense de Hockey Sobre Cesped', 'Bonaerense'),
-(27, '', 'Santa Fe', 'Asociacion Santafesina', 'NEA'),
-(28, '', 'Entre Rios', 'Federacion Entrerriana', 'NEA'),
-(29, '', 'Rio Uruguay', 'Asociacion del Hockey de Rio Uruguay', 'NEA'),
-(30, '', 'Federacion de hockey del Oeste', 'Federacion de Hockey del Oeste', 'NEA'),
-(31, '', 'Santiago del Estero', 'Federacion de Santiago del Estero', 'NOA'),
-(32, '', 'La Rioja', 'Asociación Riojana', 'NOA'),
-(33, '', 'Corrientes', 'Asociacion Correntina', 'NEA'),
-(34, '', 'San Luis', 'Federación Sanluiseña', 'Centro y Cuyo'),
-(35, '', 'Chaco', 'Federacion Chaqueña', 'NEA');
+INSERT INTO `asociaciones` (`id_asociacion`, `logo`, `asociacion`, `region`) VALUES
+(43, '', 'Federacion Tandilense', 'Bonaerense'),
+(44, '', 'Asociacion Marplatense', 'Bonaerense'),
+(48, '', 'Asociacion Bahiense', 'Bonaerense'),
+(49, '', 'Asociacion Cuenca del Salado', 'Bonaerense'),
+(50, '', 'Asociacion del Oeste de Bs. As.', 'Bonaerense'),
+(51, '', 'Asociacion Noroeste', 'Bonaerense'),
+(52, '', 'Asociacion Reg. Tresarroyense', 'Bonaerense'),
+(53, '', 'Federacion del Sudoeste', 'Bonaerense'),
+(54, '', 'Federacion del Centro de Bs. As.', 'Bonaerense');
 
 -- --------------------------------------------------------
 
@@ -123,13 +106,13 @@ INSERT INTO `user` (`id`, `email`, `password`) VALUES
 --
 ALTER TABLE `arbitros`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `asociacion` (`asociacion`);
+  ADD KEY `id_asociacion` (`id_asociacion`);
 
 --
--- Indices de la tabla `equipos`
+-- Indices de la tabla `asociaciones`
 --
-ALTER TABLE `equipos`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `asociaciones`
+  ADD PRIMARY KEY (`id_asociacion`);
 
 --
 -- Indices de la tabla `user`
@@ -145,19 +128,29 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `arbitros`
 --
 ALTER TABLE `arbitros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
--- AUTO_INCREMENT de la tabla `equipos`
+-- AUTO_INCREMENT de la tabla `asociaciones`
 --
-ALTER TABLE `equipos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+ALTER TABLE `asociaciones`
+  MODIFY `id_asociacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `arbitros`
+--
+ALTER TABLE `arbitros`
+  ADD CONSTRAINT `arbitros_ibfk_1` FOREIGN KEY (`id_asociacion`) REFERENCES `asociaciones` (`id_asociacion`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
